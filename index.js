fbf48824bff483ff2d1129412ca0f7173d2ae7e5
Govind Shringi <govind.shringi@qdegrees.org>
2025-04-21 14:12:59 +0530

    updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const path = require('path');
+const cors =require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,5 +12,6 @@ connectDB();
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/users', userRoutes);
+app.use(cors())
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
