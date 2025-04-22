f738c67fadac46a737ec4f771a490b67dd09f149
Govind Shringi <govind.shringi@qdegrees.org>
2025-04-22 17:19:59 +0530

    updates

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors =require('cors')
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 app.use(express.json());
